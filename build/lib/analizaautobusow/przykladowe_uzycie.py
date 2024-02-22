f40ce22810cb2905ce7fee15bed5155fc843b3b7
Andrzej Gwiazda <jedrekgwiazda@gmail.com>
2024-02-22 12:16:24 +0100

    Zrobiono pakiet

--- build/lib/analizaautobusow/przykladowe_uzycie.py
+++ build/lib/analizaautobusow/przykladowe_uzycie.py
@@ -5,9 +5,10 @@ if __name__ == "__main__":
 
     pobieranie_danych.pobierz_rozkład_jazdy("rozklad_przyklad.txt")
 
-    pobieranie_danych.pobierz_dane(60, "dane_przyklad.txt", pobieranie_danych.wczytaj_rozkład_jazdy("rozklad_21_02.txt"))
+    pobieranie_danych.pobierz_dane(60*60, "dane_22_02_0200.txt", pobieranie_danych.wczytaj_rozkład_jazdy("rozklad_21_02.txt"))
 
-    analiza_danych.analizuj("dane_przyklad.txt")
-    #  analiza_danych.analizuj("dane_21_02_1050.txt")
+    analiza_danych.analizuj("dane_21_02_1050.txt")
+    #  analiza_danych.analizuj("dane_21_02_2300.txt")
     #  analiza_danych.analizuj("dane_21_02_1200.txt")
+    #  analiza_danych.analizuj("dane_22_02_0200.txt")
     #  analiza_danych.analizuj("dane_21_02_0110.txt")
